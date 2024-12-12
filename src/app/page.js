8cd1c4f0ca62a05ba6a33ffd9dac7c195fa9b12e
yulia <langedoc@gmail.com>
2024-12-12 16:30:24 +0100

    feat: API calls using React Query, filtering first 20 unique items

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,39 @@
+"use client";
+
 import styles from './page.module.css';
+import { usePhones } from './utils/usePhones';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
+    <QueryClientProvider client={queryClient}>
+      <Content />
+    </QueryClientProvider>
+  );
+}
+
+
+const Content = () => {
+  const { data, isLoading, error } = usePhones();
+
+  if (isLoading) return null;
+  if (error) return <div>Error fetching data</div>;
+
+  /* Some of the models in the fetched data are duplicated,
+  to avoid showing the same model twice and have problems with keys
+  create Set to select unique phone IDs*/
+  const uniquePhones = Array.from(new Set(data.map(phone => phone.id)))
+    .map(id => data.find(phone => phone.id === id));
+
+  return (
     <div className={styles.page}>
-      <h1>Catalog</h1>
+      <ul>
+        {uniquePhones.map(phone => (
+          <li key={phone.id}>{phone.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/app/utils/usePhones.js
@@ -0,0 +1,15 @@
+// Since React 19 is used, version 5 of react-quer is requiered
+import { useQuery } from '@tanstack/react-query';
+import { fetchData } from './api';
+
+export const usePhones = () => {
+    // Starting from vertion 5 useQuery requeres an object instead of separate parameters
+    return useQuery ({
+        // specifies the key for the query
+        queryKey: ['phones'],
+        // function that fetches the data
+        queryFn: async () => {
+            const data = await fetchData('/');
+            return data.slice(0, 20); // Filter first 20 phones
+    }});
+}
