455dd7584098f526050c4655f06b5e9c37bafe35
yulia <langedoc@gmail.com>
2024-12-16 10:02:05 +0100

    feat: back button functionality

--- src/app/components/Back/Back.js
+++ src/app/components/Back/Back.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './Back.module.css';
 import { SlArrowLeft } from "react-icons/sl";
+import { useRouter } from 'next/navigation';
 
 export default function Back() {
+    const router = useRouter();
+
+    const handleBackClick = () => {
+        router.back();
+    };
+
     return (
         <div className={styles.container}>
-            <button className={styles.back_button}>
+            <button onClick={handleBackClick} className={styles.back_button}>
                 <SlArrowLeft className={styles.icon} />
                 BACK
             </button>
