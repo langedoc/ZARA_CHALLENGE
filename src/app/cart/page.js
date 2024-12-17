1b8302f20fef4c79b98a789ca287c90aae981119
yulia <langedoc@gmail.com>
2024-12-17 15:12:48 +0100

    feat: cart showing added items

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,9 +2,23 @@
 
 import React from "react";
 import styles from './page.module.css';
+import { useCart } from '../../context/CartContext';
+import CartItem from '../components/CartItem/CartItem';
 
 export default function CartPage() {
+    const { cart } = useCart();
+    // const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
+
     return (
-        <h1 className={styles.cart}>Shopping cart</h1>
+        <div className={styles.cart_container}>
+            <div className={styles.title_wrapper}>
+                <h1>CART ({cart.length})</h1>
+            </div>
+            <div className={styles.items_list}>
+                {cart.map((item, index) => (
+                    <CartItem key={index} item={item} />
+                ))}
+            </div>
+        </div>
     );
 }
