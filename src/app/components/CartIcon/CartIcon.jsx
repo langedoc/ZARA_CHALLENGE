08d1afb2adb17f608352b5777cfa35e09cb541a5
yulia <langedoc@gmail.com>
2024-12-18 09:34:56 +0100

    fix: Header test

--- src/app/components/CartIcon/CartIcon.jsx
+++ src/app/components/CartIcon/CartIcon.jsx
@@ -3,18 +3,20 @@
 import React from 'react';
 import Link from 'next/link';
 import { LiaShoppingBagSolid } from 'react-icons/lia';
-import { useCart } from '../../../context/CartContext';
 import styles from './CartIcon.module.css';
+import { useCart } from '../../../context/CartContext';
 
-export default function CartIcon() {
+const CartIcon = () => {
   const { cart } = useCart();
 
   return (
-    <div className={styles.cart_container}>
+    <div className={styles.cart_icon}>
       <Link href={'/cart'} aria-label="cart">
         <LiaShoppingBagSolid className={styles.icon} />
       </Link>
       <span className={styles.item_count}>{cart.length}</span>
     </div>
   );
-}
+};
+
+export default CartIcon;
